0f9d65ff11cfd79d7f85e7ec2707e358d5903265
aviv ron <aviv1ron1@gmail.com>
2018-02-26 17:32:19 +0200

    add try catch

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const fs = require('fs');
 const falafel = require('falafel');
 
 var remove = function(src, callback) {
-    var output = falafel(src, function(node) {
-        if (node.type == "ExpressionStatement" && node.expression.type === 'CallExpression' && node.expression.callee.type == "MemberExpression" && node.expression.callee.object.name == "console") {
-            if (src.substr(node.end, 12).indexOf("//include") < 0) {
-                node.update('');
+    try {
+        var output = falafel(src, function(node) {
+            if (node.type == "ExpressionStatement" && node.expression.type === 'CallExpression' && node.expression.callee.type == "MemberExpression" && node.expression.callee.object.name == "console") {
+                if (src.substr(node.end, 12).indexOf("//include") < 0) {
+                    node.update('');
+                }
             }
-        }
-    });
-    callback(null, output);
+        });
+        callback(null, output);
+    } catch (ex) {
+        console.error("remove-log: error while walking code", err);
+        callback(err);
+    }
 }
 
 module.exports = {
     removeFromPath: (path, callback) => {
         fs.readFile(path, (err, data) => {
             if (err) {
-                console.error("node-remove-log: file not found", err);
+                console.error("remove-log: file not found", err);
                 callback(err);
             } else {
                 var src = data.toString();
